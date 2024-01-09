827b887f14584ba16280b1c68718a0bbf99fae4a
SzGabor <gabor.szendrei1@gmail.com>
2024-01-09 23:41:59 +0100

    Fix capitalization in skills.css import

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
-import '../styles/Skills.css';
+import '../styles/skills.css';
 
 gsap.registerPlugin(ScrollTrigger);
 
