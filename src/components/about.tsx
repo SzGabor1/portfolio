1481650443b6ef2e7b467a0452d822212ad1e9ea
SzGabor <gabor.szendrei1@gmail.com>
2024-02-21 13:53:09 +0100

    Remove unnecessary paragraph in About component

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -6,8 +6,7 @@ const About = () => {
 
         <h2>About Me</h2>
         <p>I am Gábor Szendrei, currently studying Software Information Technology BSC at the University of Miskolc and i'm a trainee at Hungarian National Tax and Customs Administration.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate officia autem tempore eius omnis facere voluptatibus. Porro, aliquid? Dolores saepe repudiandae facere aperiam iste pariatur alias autem. Maxime, ratione modi.</p>
-    </div>
+   </div>
 
   );
 };
